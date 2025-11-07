6e796f967df7d941ce2e2978aafb2405895890ca
pallavi-doctom <chittimallapalla1@gmail.com>
2025-11-07 19:46:33 +0530

    Update AuthController.java

--- src/main/java/com/klu/ecommerce/controller/AuthController.java
+++ src/main/java/com/klu/ecommerce/controller/AuthController.java
@@ -3,12 +3,15 @@ package com.klu.ecommerce.controller;
 import com.klu.ecommerce.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin
 public class AuthController {
+
     private final UserService userService;
 
     public AuthController(UserService userService) {
@@ -16,17 +19,30 @@ public class AuthController {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<String> registerUser(@RequestBody Map<String, String> request) {
+    public ResponseEntity<Map<String, Object>> registerUser(@RequestBody Map<String, String> request) {
         String username = request.get("username");
         String email = request.get("email");
         String password = request.get("password");
-        return ResponseEntity.ok(userService.registerUser(username, email, password));
+
+        String result = userService.registerUser(username, email, password);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", result);
+
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> loginUser(@RequestBody Map<String, String> request) {
+    public ResponseEntity<Map<String, Object>> loginUser(@RequestBody Map<String, String> request) {
         String username = request.get("username");
         String password = request.get("password");
-        return ResponseEntity.ok(userService.loginUser(username, password));
+
+        String result = userService.loginUser(username, password);
+
+        // you can also return token here
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", result);
+
+        return ResponseEntity.ok(response);
     }
 }
