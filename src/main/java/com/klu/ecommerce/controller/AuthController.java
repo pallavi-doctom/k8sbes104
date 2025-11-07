b9d97a732acc1e21f3f556d3ce2376b0a341ce12
pallavi-doctom <chittimallapalla1@gmail.com>
2025-11-07 19:55:10 +0530

    Update AuthController.java

--- src/main/java/com/klu/ecommerce/controller/AuthController.java
+++ src/main/java/com/klu/ecommerce/controller/AuthController.java
@@ -1,48 +1,17 @@
-package com.klu.ecommerce.controller;
+import axios from "axios";
 
-import com.klu.ecommerce.service.UserService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+const API_URL = "http://localhost:30083/back1/auth";
 
-import java.util.HashMap;
-import java.util.Map;
+export const login = async (username, password) => {
+  const response = await axios.post(`${API_URL}/login`, { username, password });
+  localStorage.setItem("token", response.data);
+  return response.data;
+};
 
-@RestController
-@RequestMapping("/auth")
-@CrossOrigin
-public class AuthController {
+export const signup = async (username, email, password) => {
+  return axios.post(`${API_URL}/signup`, { username, email, password });
+};
 
-    private final UserService userService;
-
-    public AuthController(UserService userService) {
-        this.userService = userService;
-    }
-
-    @PostMapping("/signup")
-    public ResponseEntity<Map<String, Object>> registerUser(@RequestBody Map<String, String> request) {
-        String username = request.get("username");
-        String email = request.get("email");
-        String password = request.get("password");
-
-        String result = userService.registerUser(username, email, password);
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("message", result);
-
-        return ResponseEntity.ok(response);
-    }
-
-    @PostMapping("/login")
-    public ResponseEntity<Map<String, Object>> loginUser(@RequestBody Map<String, String> request) {
-        String username = request.get("username");
-        String password = request.get("password");
-
-        String result = userService.loginUser(username, password);
-
-        // you can also return token here
-        Map<String, Object> response = new HashMap<>();
-        response.put("message", result);
-
-        return ResponseEntity.ok(response);
-    }
-}
+export const logout = () => {
+  localStorage.removeItem("token");
+};
